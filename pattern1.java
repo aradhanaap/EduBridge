dca053f583e07e63cfaac3aa4dc47b750be5576f
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-23 19:27:32 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,18 @@
+
+    public class pattern1 {
+static void add(int...nums) {
+	int sum=0;
+	for(int num:nums) {
+		sum+=num;}
+	System.out.println(sum);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(1,2,3,4);
+
+	}
+
+
+
+    
+}
