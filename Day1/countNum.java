f65def17e0ffa352947e93411534d17acf80be1b
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-25 12:13:38 +0530

    Add files via upload

---
+++ Day1/countNum.java
@@ -0,0 +1,10 @@
+package Dsa;
+
+public class countNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day1/evenNum.java
@@ -0,0 +1,18 @@
+package Dsa;
+
+public class evenNum {
+
+	public static void main(String[] args) {
+		int i=2;
+		
+		
+		while(i<=50) {
+			System.out.println(i+" ");
+			i+=2;
+			
+		}
+	    
+
+	}
+
+}
