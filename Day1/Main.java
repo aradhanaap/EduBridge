a5078f03cf85f5b52e44e3adeaa6e67135251a79
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-25 12:11:02 +0530

    Update Main.java

--- Day1/Main.java
+++ Day1/Main.java
@@ -1,1 +1,13 @@
+public class Main {
+	public static void printNum(int a,int b) {
+		if(b==5)
+			return;
+		System.out.println(a);
+		printNum(a+2,b+1);
+		
+	}
+	public static void main(String[] args) {
+		printNum(0,0);
+	}
 
+}
