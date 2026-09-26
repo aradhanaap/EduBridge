0eac030e5465362955eada27cbb020186229f0b3
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 21:52:34 +0530

    Add files via upload

---
+++ Day2/SumEvenNum.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class SumEvenNum {
+
+	public static void main(String[] args) {
+	int sum=0;
+	int i=2;
+	while(i<=100)
+	{
+		if(i%2==0)
+			sum+=i;
+		i++;
+	}
+	System.out.println("Sum of even numbers from 1 to 100 is =" + sum);
+			
+	}
+
+	}
+
+
