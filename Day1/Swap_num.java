c032b63a466a5ca57023ee0585d4cb3f127c4b74
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 19:26:40 +0530

    Add files via upload

---
+++ Day1/Swap_num.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Swap_num {
+
+	public static void main(String[] args) {
+		int a=15,b=40;
+		int temp;
+		System.out.println("Value before swspping a=" + a + "b=" + b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("value after swapping a=" +a+ "b=" + b);
+
+	}
+
+}
