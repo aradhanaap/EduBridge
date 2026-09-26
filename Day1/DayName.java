13d240dd0954bf5347f9143a5f4bc4cc3dd001ed
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 21:11:31 +0530

    Add files via upload

---
+++ Day1/DayName.java
@@ -0,0 +1,32 @@
+package day1;
+import java.util.Scanner;
+
+public class DayName {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your choice");
+		int num=sc.nextInt();
+		switch(num)
+		{
+		case 1:System.out.println("Mondey");
+		break;
+		case 2:System.out.println("Tuesday");
+		break;
+		case 3:System.out.println("Wednesday");
+		break;
+		case 4:System.out.println("Thursday");
+		break;
+		case 5:System.out.println("Friday");
+		break;
+		case 6:System.out.println("Sturday");
+		break;
+		case 7:System.out.println("sunday");
+		break;
+		default:System.out.println("Invalid day");
+		}
+		
+
+	}
+
+}
