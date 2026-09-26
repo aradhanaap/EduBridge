610cc7f3913205e1f69cac9d425bbafafebbb17a
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 19:18:56 +0530

    Add files via upload

---
+++ Day1/Hello.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("ABOUT ME:");
+		System.out.println("Name: Aradhana Pradhan");
+		System.out.println("Course: Java Program");
+		System.out.println("College Vijaya vittala intitute of technology");
+		
+		
+
+	}
+
+}
