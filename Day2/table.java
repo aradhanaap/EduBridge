c264a09b0c36cfb5f65c70d60f93542a8b7dadf2
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 22:16:43 +0530

    Add files via upload

---
+++ Day2/table.java
@@ -0,0 +1,20 @@
+package Dsa;
+
+
+public class table {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the number");
+		int n=9;
+		System.out.println("multiplication table");
+		for(int i=0;i<=10;i++)
+		{
+			int result=i*n;
+			System.out.println(i + "*" + "9" + "=" + result);
+		}
+			
+
+	}
+
+}
