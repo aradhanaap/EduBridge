c3bf5abfe61d85f6ae83cde62d5a4f7db0e86407
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 20:39:46 +0530

    Add files via upload

---
+++ Day1/LargeNum.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class LargeNum {
+
+	public static void main(String[] args) {
+		int a=45,b=89,c=23;
+		if(a>b && a>c)
+			System.out.println("Largest num is a=45");
+		else if(b>a && b>c)
+		    System.out.println("Largest num is b=89");
+		else
+		    System.out.println("Largest num is c=23");
+
+	}
+
+}
