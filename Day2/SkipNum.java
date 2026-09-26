4ab0bca5ca9445b90fd227a89e3b6f446e7ee00d
aradhanaap <pradhanaradhana91@gmail.com>
2026-09-26 21:38:39 +0530

    Add files via upload

---
+++ Day2/SkipNum.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class SkipNum {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=30;i++) {
+			if(i%4==0)
+				continue;
+			
+		
+			System.out.println(i);
+		}
+
+	}
+
+}
